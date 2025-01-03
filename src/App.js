9bafb77b59f5426ba6e35dc5101bf41e037d96d0
am12bas <ambaseeth@gmail.com>
2025-01-03 16:58:39 +0530

    feat: initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import SurveyForm from "./components/SurveyForm";
+import "./App.css"
+
+const App = () => {
+  return (
+    <div>
+      <SurveyForm />
+    </div>
+  );
+};
+
+export default App;
